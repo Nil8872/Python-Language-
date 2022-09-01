a726424f6fd970ff3beb024cb6119b67cd8e196c
Nilesh Sariya <sariyanilesh7265@gmail.com>
2022-09-01 18:25:53 +0530

    Dictionary in python

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+d2 = {"Nilesh":"Bhajiya", "Vijay":"Bhindi", "Umesh": "laptop", "120":"Computer", "Nil":{"A":"1", "B":"2", "C":"3"}}
+
+# print(type(d2))
+# print(d2);
+# d2["Rajesh"] = "Cricketer"
+# print(d2)
+# # d2.update("Mukesh","Programmer")
+# del d2["Rajesh"]
+
+# print(d2)
+# d3 = d2.copy()
+# del d3["Nilesh"]
+# print(d3)
+# print(d2)
+# print(d2)
+# d2.update({"Bhavesh": "Farmer"})
+# print(d2)
+print(d2.keys())
+print(d2.items())
---
+++ list.py
@@ -0,0 +1,23 @@
+# stu_name = ["Nilesh", "Vijay", "Umesh", "Ramji", "Tushar", "Ankit", "Harsh","Mohin", "Bhargav"]
+
+# print(stu_name[1])
+# stu_name.pop()
+# stu_name.pop()
+# stu_name.pop()
+# print(stu_name)
+# stu_name.append("Bhavesh")
+# print(stu_name)
+
+# # stu_name.reverse();
+# # stu_name.insert(3,"Kali Linux")
+# stu_name.remove("Nilesh")
+# stu_name.remove("Vijay")
+# # stu_name.remove()
+# print(stu_name)
+
+number = [2,34,55,22,6,45,3]
+print(number)
+# number.pop()
+number.sort()
+number.reverse()
+print(number)
