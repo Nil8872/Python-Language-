5e7b0fa9fa639963f8102317e8bb8bd220bf4923
Nilesh Sariya <sariyanilesh7265@gmail.com>
2022-09-01 16:00:15 +0530

    String mehtods

---
+++ String.py
@@ -0,0 +1,23 @@
+mystr = "my name is Sariya Nilesh"
+# print(mystr)
+# print(mystr[::-1])
+# print(mystr.upper())
+# print(mystr.lower())
+# print(mystr.capitalize())
+# print(mystr.endswith("Nil"))
+# print(mystr.casefold())
+# print(mystr.count("a"))
+
+# print(mystr.encode())
+# print(mystr.encode())
+# print(mystr.center(8))
+# print(mystr.isalnum())
+# print(mystr.isalpha())
+# print(mystr.index('m'))
+# print(mystr.rindex('m'))
+# print(mystr.replace("my","Your"))
+# print(mystr, end=" ")
+# print(mystr.ljust(10), end=" ")
+y = "hello How are your"
+x = "Nilesh"
+print(y.ljust(50), x, mystr.rjust(50))
